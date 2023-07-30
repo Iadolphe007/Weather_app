2e36bc2b2acb9ccbf559d5d9af124c57e4387e44
Iadolphe007 <iadolphe007@gmail.com>
2023-07-30 20:21:26 +0200

    updating script

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const api_url= 'https://api.openweathermap.org/data/2.5/weather?q={city name}&ap
 
 const search_input = document.querySelector('.search-input').value
 const search_btn = document.querySelector('.search-btn')
+const weather_icon = document.querySelector('.rain-icon')
 // =&units=metric
 
 
@@ -10,14 +11,44 @@ const search_btn = document.querySelector('.search-btn')
 
 async function check_weather(city){
     const response = await fetch(api_url + city + `$appid=${api_key}`)
-    let data = await response.json()
     
-    document.getElementById('#location').innerHTML = data.name
-    document.getElementById('#temperature').innerHTML =Math.round(data.main.temp)
-    document.querySelector('.humidity').innerHTML = data.main.humidity
-    document.querySelector('.wind').innerHTML = data.wind.speed
+
+    if(response.status == 404){
+        document.querySelector('.error-input').style.siplay = 'block'
+        document.querySelector('.current-weather').style.siplay = 'none'
+    }
+    else{
+        let data = await response.json()
+        document.getElementById('#location').innerHTML = data.name
+        document.getElementById('#temperature').innerHTML =Math.round(data.main.temp)
+        document.querySelector('.humidity').innerHTML = data.main.humidity
+        document.querySelector('.wind').innerHTML = data.wind.speed
+        document.querySelector('.current-weather').style.siplay = 'block'
+        document.querySelector('.error-input').style.siplay = 'none'
+        // check_weather_condition()
+        // display()
+    }
 }
 
 search_btn.addEventListener('click', ()=>{
     check_weather(search_input)
 })
+
+function check_weather_condition(){
+    if(data.weather[0].main == 'cloud'){
+        weather_icon.src = 'image/clouds.png'
+    } else if(data.weather[0].main == 'clear'){
+        weather_icon.src = 'image/clear.png'
+    } else if(data.weather[0].main == 'rain'){
+        weather_icon.src = 'image/rain.png'
+    } else if(data.weather[0].main == 'Drizzle'){
+        weather_ico.src = 'image/Drizzle.png'
+    } else if(data.weather[0].main == 'Mist'){
+        weather_icon.src = 'image/Mist.png'
+    }
+}
+
+// function display(){
+//     document.querySelector('.current-weather').style.siplay = 'block'
+// document.querySelector('.error-input').style.siplay = 'none'  
+// }
