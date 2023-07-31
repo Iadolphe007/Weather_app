a9e10c056aa41fd9df36541772883e1542efb6c9
Iadolphe007 <iadolphe007@gmail.com>
2023-07-31 09:57:37 +0200

    updated script

--- index.js
+++ index.js
@@ -1,54 +1,42 @@
 const api_key = '3188dfc0bf210fa37d56de756b126b8c'
-const api_url= 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+const api_url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
-const search_input = document.querySelector('.search-input').value
+const search_input = document.querySelector('.search-input')
 const search_btn = document.querySelector('.search-btn')
 const weather_icon = document.querySelector('.rain-icon')
-// =&units=metric
-
-
 
 
 async function check_weather(city){
-    const response = await fetch(api_url + city + `$appid=${api_key}`)
+    const response = await fetch(api_url + city + `&appid=${api_key}`)
     
 
     if(response.status == 404){
-        document.querySelector('.error-input').style.siplay = 'block'
-        document.querySelector('.current-weather').style.siplay = 'none'
+        document.querySelector('.current-weather').style.dsiplay = 'none'
+        alert('invalid city name/ check your spelling')
     }
     else{
         let data = await response.json()
-        document.getElementById('#location').innerHTML = data.name
-        document.getElementById('#temperature').innerHTML =Math.round(data.main.temp)
-        document.querySelector('.humidity').innerHTML = data.main.humidity
-        document.querySelector('.wind').innerHTML = data.wind.speed
-        document.querySelector('.current-weather').style.siplay = 'block'
-        document.querySelector('.error-input').style.siplay = 'none'
-        // check_weather_condition()
-        // display()
+        console.log(data)
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + 'kmph'
+
+        if(data.weather[0].main == 'Clouds'){
+            weather_icon.src = 'image/clouds.png'
+        } else if(data.weather[0].main == 'Clear'){
+            weather_icon.src = 'image/clear.png'
+        } else if(data.weather[0].main == 'rain'){
+            weather_icon.src = 'image/rain.png'
+        } else if(data.weather[0].main == 'Drizzle'){
+            weather_icon.src = 'image/drizzle.png'
+        } else if(data.weather[0].main == 'Mist'){
+            weather_icon.src = 'image/mist.png'
+        }
+        document.querySelector('.current-weather').style.display = 'block'
     }
 }
 
 search_btn.addEventListener('click', ()=>{
-    check_weather(search_input)
+    check_weather(search_input.value)
 })
-
-function check_weather_condition(){
-    if(data.weather[0].main == 'cloud'){
-        weather_icon.src = 'image/clouds.png'
-    } else if(data.weather[0].main == 'clear'){
-        weather_icon.src = 'image/clear.png'
-    } else if(data.weather[0].main == 'rain'){
-        weather_icon.src = 'image/rain.png'
-    } else if(data.weather[0].main == 'Drizzle'){
-        weather_ico.src = 'image/Drizzle.png'
-    } else if(data.weather[0].main == 'Mist'){
-        weather_icon.src = 'image/Mist.png'
-    }
-}
-
-// function display(){
-//     document.querySelector('.current-weather').style.siplay = 'block'
-// document.querySelector('.error-input').style.siplay = 'none'  
-// }
